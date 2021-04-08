57820421a08d66f7cd07ea4bac5989d995970e22
aisolab <bsk0130@gmail.com>
2021-04-08 13:35:33 +0000

    [python] feat: Add MorphemePreTokenizer - Mecab 기반의 MorphemePreTokenizer를 추가
    Refs: #1

--- src/pre_tokenizer.py
+++ src/pre_tokenizer.py
@@ -1,6 +1,7 @@
 import re
 from typing import Dict, List
 
+from konlpy.tag import Mecab
 from kss import split_sentences
 
 WHITESPACE = re.compile(r"\s+")
@@ -11,22 +12,65 @@ class WhitespacePreTokenizer:
         self._input_key = input_key
         self._output_key = output_key
         self._target = target
+        self._split = WHITESPACE.split
 
     def pretokenize(self, string: str) -> List[str]:
         list_of_tokens = []
         list_of_sentences = split_sentences(string)
 
-        for sentence in list_of_sentences:
-            if self._target == "bbpe":
-                list_of_footprints: List[str] = WHITESPACE.split(sentence)
+        if self._target == "bbpe":
+            for sentence in list_of_sentences:
+                list_of_eojeols: List[str] = self._split(sentence)
 
-                for idx, footprint in enumerate(list_of_footprints):
+                for idx, eojeol in enumerate(list_of_eojeols):
                     if idx != 0:
-                        list_of_tokens.append(" " + footprint)
+                        list_of_tokens.append(" " + eojeol)
                     else:
-                        list_of_tokens.append(footprint)
-            else:
-                list_of_tokens.extend(WHITESPACE.split(sentence))
+                        list_of_tokens.append(eojeol)
+        else:
+            for sentence in list_of_sentences:
+                list_of_tokens.extend(self._split(sentence))
+
+        if list_of_tokens:
+            return list_of_tokens
+        else:
+            return [""]
+
+    def process(self, example: Dict[str, List[str]]) -> Dict[str, List[str]]:
+        example[self._input_key] = self.pretokenize(example[self._input_key])
+        return example
+
+
+class MorphemePreTokenizer:
+    def __init__(self, target="cbpe", input_key="text", output_key="text") -> None:
+        self._input_key = input_key
+        self._output_key = output_key
+        self._target = target
+        self._split = Mecab().morphs
+
+    def pretokenize(self, string: str) -> List[str]:
+        list_of_tokens = []
+        list_of_sentences = split_sentences(string)
+
+        if self._target == "bbpe":
+            for sentence in list_of_sentences:
+                list_of_eojeols: List[str] = WHITESPACE.split(sentence)
+
+                for i, eojeol in enumerate(list_of_eojeols):
+                    list_of_morphems = self._split(eojeol)
+
+                    if i == 0:
+                        for j, morpheme in enumerate(list_of_morphems):
+                            list_of_tokens.append(morpheme)
+                    else:
+                        for j, morpheme in enumerate(list_of_morphems):
+                            if j == 0:
+                                list_of_tokens.append(" " + morpheme)
+                            else:
+                                list_of_tokens.append(morpheme)
+        else:
+            for sentence in list_of_sentences:
+                list_of_tokens.extend(self._split(sentence))
 
         if list_of_tokens:
             return list_of_tokens
--- tests/test_pre_tokenizer.py
+++ tests/test_pre_tokenizer.py
@@ -1,4 +1,4 @@
-from src.pre_tokenizer import WhitespacePreTokenizer
+from src.pre_tokenizer import MorphemePreTokenizer, WhitespacePreTokenizer
 
 
 def test_WhitespacePreTokenizer():
@@ -6,20 +6,20 @@ def test_WhitespacePreTokenizer():
     test_case02 = "안녕하세요."
     test_case03 = ""
 
-    pretokenizer_for_cbpe = WhitespacePreTokenizer(target="cbpe")
-    assert pretokenizer_for_cbpe.pretokenize(test_case01) == [
+    pretokenizer_not_bbpe = WhitespacePreTokenizer(target="cbpe")
+    assert pretokenizer_not_bbpe.pretokenize(test_case01) == [
         "안녕하세요,",
         "제",
         "이름은",
         "김보섭입니다.",
     ]
-    assert pretokenizer_for_cbpe.pretokenize(test_case02) == ["안녕하세요."]
-    assert pretokenizer_for_cbpe.pretokenize(test_case03) == [""]
-    assert pretokenizer_for_cbpe.process({"text": test_case01}) == {
+    assert pretokenizer_not_bbpe.pretokenize(test_case02) == ["안녕하세요."]
+    assert pretokenizer_not_bbpe.pretokenize(test_case03) == [""]
+    assert pretokenizer_not_bbpe.process({"text": test_case01}) == {
         "text": ["안녕하세요,", "제", "이름은", "김보섭입니다."]
     }
-    assert pretokenizer_for_cbpe.process({"text": test_case02}) == {"text": ["안녕하세요."]}
-    assert pretokenizer_for_cbpe.process({"text": test_case03}) == {"text": [""]}
+    assert pretokenizer_not_bbpe.process({"text": test_case02}) == {"text": ["안녕하세요."]}
+    assert pretokenizer_not_bbpe.process({"text": test_case03}) == {"text": [""]}
 
     pretokenizer_for_bbpe = WhitespacePreTokenizer(target="bbpe")
     assert pretokenizer_for_bbpe.pretokenize(test_case01) == [
@@ -34,4 +34,56 @@ def test_WhitespacePreTokenizer():
         "text": ["안녕하세요,", "제", " 이름은", " 김보섭입니다."]
     }
     assert pretokenizer_for_bbpe.process({"text": test_case02}) == {"text": ["안녕하세요."]}
+    assert pretokenizer_for_bbpe.process({"text": test_case03}) == {"text": [""]}
+
+
+def test_MorphemePreTokenizer():
+    test_case01 = "안녕하세요, 제 이름은 김보섭입니다."
+    test_case02 = "안녕하세요."
+    test_case03 = ""
+
+    pretokenizer_not_bbpe = MorphemePreTokenizer(target="cbpe")
+    assert pretokenizer_not_bbpe.pretokenize(test_case01) == [
+        "안녕",
+        "하",
+        "세요",
+        ",",
+        "제",
+        "이름",
+        "은",
+        "김보섭",
+        "입니다",
+        ".",
+    ]
+    assert pretokenizer_not_bbpe.pretokenize(test_case02) == ["안녕", "하", "세요", "."]
+    assert pretokenizer_not_bbpe.pretokenize(test_case03) == [""]
+    assert pretokenizer_not_bbpe.process({"text": test_case01}) == {
+        "text": ["안녕", "하", "세요", ",", "제", "이름", "은", "김보섭", "입니다", "."]
+    }
+    assert pretokenizer_not_bbpe.process({"text": test_case02}) == {
+        "text": ["안녕", "하", "세요", "."]
+    }
+    assert pretokenizer_not_bbpe.process({"text": test_case03}) == {"text": [""]}
+
+    pretokenizer_for_bbpe = MorphemePreTokenizer(target="bbpe")
+    assert pretokenizer_for_bbpe.pretokenize(test_case01) == [
+        "안녕",
+        "하",
+        "세요",
+        ",",
+        "제",
+        " 이름",
+        "은",
+        " 김보섭",
+        "입니다",
+        ".",
+    ]
+    assert pretokenizer_for_bbpe.pretokenize(test_case02) == ["안녕", "하", "세요", "."]
+    assert pretokenizer_for_bbpe.pretokenize(test_case03) == [""]
+    assert pretokenizer_for_bbpe.process({"text": test_case01}) == {
+        "text": ["안녕", "하", "세요", ",", "제", " 이름", "은", " 김보섭", "입니다", "."]
+    }
+    assert pretokenizer_for_bbpe.process({"text": test_case02}) == {
+        "text": ["안녕", "하", "세요", "."]
+    }
     assert pretokenizer_for_bbpe.process({"text": test_case03}) == {"text": [""]}
